2a92d08e453ace7d2059c68c43f2534825a8a0fa
Mohamed Abdel-Samie <esmaelm885@gmail.com>
2022-05-21 20:33:56 +0200

    fix not find the thumbnial directory

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import fsPromises from 'fs/promises';
 import { getImgFilesFromDir } from '../modules/fileSystem.module';
 
 export const home = (req: express.Request, res: express.Response) => {
@@ -8,6 +11,7 @@ export const home = (req: express.Request, res: express.Response) => {
 };
 
 export const thumbnailImage = (req: express.Request, res: express.Response) => {
+  mkThumbnailsDirIfNotExist();
   getImgFilesFromDir('public/assets/images/thumbnails/', (files) => {
     res.render('pages/thumbnail-image', { files });
   });
@@ -17,4 +21,21 @@ export const gallery = (req: express.Request, res: express.Response) => {
   getImgFilesFromDir('public/assets/images/gallery/', (files) => {
     res.render('pages/gallery', { files });
   });
+};
+
+const mkThumbnailsDirIfNotExist = async () => {
+  try {
+    if (
+      !fs.existsSync(
+        path.join(__dirname, '../../public/assets/images/thumbnails')
+      )
+    ) {
+      await fsPromises.mkdir(
+        path.join(__dirname, '../../public/assets/images/thumbnails')
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
